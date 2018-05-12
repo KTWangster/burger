4ab1695a86fdd8d1462d31a38fb208b5efc84fb6
Katie Wang <ktwang16@mac.com>
2018-05-12 15:47:23 -0400

    added js assets

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,43 @@
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+        var id = $(this).data("id");
+        var newSleep = $(this).data("newdevour");
+
+        var newDevouredState = {
+            devoured: newDevour
+        };
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(
+            function() {
+                console.log("changed devoured to", newDevour);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function(event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        var newBurger = {
+            name: $("#burgerName").val().trim(),
+            devoured: $("[name=burger_name]").val().trim()
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("created new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
